68f4868a7630dadc404790c46b55cb1b054180f3
ameya74 <63583593+ameya74@users.noreply.github.com>
2022-02-03 12:33:43 +0530

    Stream commit

---
+++ email/httpStream.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer((req, res) => {
+    const fileStream = fs.createReadStream(
+        './content/big.txt',
+        'utf-8'
+    )
+
+    fileStream.on('open', () => {
+        fileStream.pipe(res)
+    })
+
+    fileStream.on('error', (err) => {
+        res.end(err);
+    })
+}).listen(8080)
