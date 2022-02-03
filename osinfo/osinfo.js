1b37a30184a58bf7026b659cad7db246b3c0df0a
ameya74 <63583593+ameya74@users.noreply.github.com>
2022-02-03 11:08:27 +0530

    Commit

--- osinfo/osinfo.js
+++ osinfo/osinfo.js
@@ -1,13 +1,5 @@
 const os = require('os');
 
-// const osinfo = () => {
-//     return {
-//         osarch: os.arch(),
-//         cpu: os.cpus(),
-//         network: os.networkInterfaces(),
-//     }
-// };
-
 const osinfo = {
     cpu: os.cpus(),
     network: os.networkInterfaces(),
